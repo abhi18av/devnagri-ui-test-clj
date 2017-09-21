91ef5b017038eb5b2942bb03a78a0ef08e16fec0
Abhinav Sharma <abhi18av@gmail.com>
2017-09-21 18:36:16 +0530

    will create a prototype of the UI testing using ammonite

--- _resources/ammonite-jbrowser.scala
+++ _resources/ammonite-jbrowser.scala
@@ -1,24 +1,22 @@
 
 import $ivy.`com.machinepublishers:jbrowserdriver:0.17.9`
-
 import com.machinepublishers
-
 import org.openqa.selenium.WebDriver;
 import com.machinepublishers.jbrowserdriver.Timezone;
 import com.machinepublishers.jbrowserdriver.JBrowserDriver;
 import com.machinepublishers.jbrowserdriver.Settings
 
 
-// JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build()); 
+// JBrowserDriver driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build());
 
 
 
-val driver = new JBrowserDriver(Settings.builder().timezone(Timezone.AMERICA_NEWYORK).build());
+val driver = new JBrowserDriver(Settings.builder().headless(false).timezone(Timezone.AMERICA_NEWYORK).build());
 
 
 // This will block for the page load and any
 // associated AJAX requests
-driver.get("http://example.com");
+driver.get("http://google.com");
 
 // You can get status code unlike other Selenium drivers.
 // It blocks for AJAX requests and page loads after clicks
@@ -27,7 +25,7 @@ println(driver.getStatusCode());
 
 // Returns the page source in its current state, including
 // any DOM updates that occurred after page load
-println(driver.getPageSource());
+//println(driver.getPageSource());
 
 // Close the browser. Allows this thread to terminate.
 driver.quit();
